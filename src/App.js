f15049f9ee0c7e99207cb36d25337da51d24e0f4
baboiletta <54394072+baboiletta@users.noreply.github.com>
2019-11-04 17:26:03 +0700

    chaydichochi

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Cars from './components/car';
+
+function App() {
+  return (
+    <div className="App">
+     <Cars></Cars>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/car.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Cars extends Component {
+    render() {
+        return (
+            <div className="car">
+                <div class="box"></div>
+                <div class="bxe">
+                    <div class="s _1"></div>
+                    <div class="s _2"></div>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Cars;
