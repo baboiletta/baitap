4246cc86d2d66fbd40326737a7afb7da6886d564
baboiletta <54394072+baboiletta@users.noreply.github.com>
2019-11-06 17:56:05 +0700

    loading

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Cars from './components/car';
 import Batquai from './components/batquai';
 import Bapbenh from './components/bapbenh';
+import Music from './components/music';
 
 function App() {
   return (
     <div className="App">
      {/* <Cars></Cars> */}
      {/* <Batquai></Batquai> */}
-     <Bapbenh></Bapbenh>
+     <Music></Music>
     </div>
   );
 }
---
+++ src/components/music.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Music extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <ul className="music">
+                    <li className="cot">
+                        <div className="d _1"></div>
+                    </li>
+                    <li className="cot">
+                        <div className="d _2"></div>
+                    </li>
+                    <li className="cot">
+                        <div className="d _3"></div>
+                    </li>
+                    <li className="cot ">
+                        <div className="d _4"></div>
+                    </li>
+                    <li className="cot ">
+                        <div className="d _5"></div>
+                    </li>
+                </ul>
+            </React.Fragment>
+
+        );
+    }
+}
+
+export default Music;
